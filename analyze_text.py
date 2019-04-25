401a876e07c05a6980787ffb04201ce71cdc4ed6
pkurjanowicz <pkurjanowicz5@gmail.com>
2019-04-24 18:50:21 -0600

    Update analyze_text.py

--- analyze_text.py
+++ analyze_text.py
@@ -1,4 +1,24 @@
 # Remember to reach out for help after your own due diligence
 
+def remove_special_chars(text):
+    special_chars = "!,'; 0)"
+    no_special_chars = ""
+    if text.isalpha():
+        return text
+    else:
+        for each_char in text:
+            if each_char not in special_chars:
+                no_special_chars = no_special_chars + each_char
+        return no_special_chars
+            
+
 def analyze_text(text):
-    # Your code here
+    text = remove_special_chars(text)
+    num_of_char = len(text)
+    letter_e = 'e'
+    letter_E = 'E'
+    total_es = text.count(letter_E) + text.count(letter_e)
+    text.isalpha()
+    num = (total_es/num_of_char)/1.0
+    percentage = "{:.2%}".format(num)
+    return ("The text contains {} alphabetic characters, of which {} ({}) are 'e'." .format(num_of_char, total_es, percentage))
